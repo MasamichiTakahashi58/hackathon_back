ef5ae693fa4d19f11b344cf76a97649ff9c9b718
masamichi <tm1124ika@gmail.com>
2024-11-18 19:44:33 +0900

    second

--- main.go
+++ main.go
@@ -7,27 +7,28 @@ import (
     "hackathon_back/db"
 )
 
+
+
 func main() {
-    // データベース接続の初期化
     if err := db.ConnectDB(); err != nil {
         log.Fatal("Could not connect to database")
     }
     defer db.DB.Close()
 
-    // プロフィール関連
+    // プロフィール
     http.HandleFunc("/profile/update", controller.UpdateProfileHandler)
 
-    // 投稿関連
+    // 投稿
     http.HandleFunc("/post/create", controller.CreatePostHandler)
     http.HandleFunc("/post/get", controller.GetPostsHandler)
     http.HandleFunc("/post/delete", controller.DeletePostHandler)
 
-    // いいね関連
+    // いいね
     http.HandleFunc("/like/add", controller.AddLikeHandler)
     http.HandleFunc("/like/remove", controller.RemoveLikeHandler)
     http.HandleFunc("/like/count", controller.CountLikesHandler)
 
-    // リプライ関連
+    // リプライ
     http.HandleFunc("/reply/create", controller.CreateReplyHandler)
     http.HandleFunc("/reply/get", controller.GetRepliesHandler)
     http.HandleFunc("/reply/delete", controller.DeleteReplyHandler)
